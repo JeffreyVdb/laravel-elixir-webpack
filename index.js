22c413dc1a88d98ecf72ed89ca4780d7e32f2ebb
Jeffrey Vandenborne <vandenbornejeffrey@gmail.com>
2015-04-05 15:49:24 +0200

    added feature to specify multiple files

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
-var gulp = require('gulp'),
-    webpack = require('gulp-webpack'),
-    gulpIf = require('gulp-if'),
-    uglify = require('gulp-uglify'),
-    _ = require('underscore'),
-    elixir = require('laravel-elixir'),
-    utilities = require('laravel-elixir/ingredients/commands/Utilities'),
+var gulp         = require('gulp'),
+    webpack      = require('gulp-webpack'),
+    uglify       = require('gulp-uglify'),
+    vinylNamed   = require('vinyl-named'),
+    gulpIf       = require('gulp-if'),
+    _            = require('lodash'),
+    elixir       = require('laravel-elixir'),
+    utilities    = require('laravel-elixir/ingredients/commands/Utilities'),
     notification = require('laravel-elixir/ingredients/commands/Notification');
 
-elixir.extend('webpack', function (src, options) {
+elixir.extend('webpack', function (src, dest, options) {
 
-    var config = this,
-        defaultOptions = {
-            debug:     ! config.production,
-            watch:     ! config.production,
-            srcDir:    config.assetsDir + 'js',
-            outputDir: config.jsOutput,
-        };
+  var config = this,
+      defaultOptions = {
+        debug: !config.production,
+        watch: !config.production,
+        srcDir: config.assetsDir + 'js',
+        base: config.assetsDir + 'js',
+        plugin: {}
+      };
 
-    options = _.extend(defaultOptions, options);
-    src = "./" + utilities.buildGulpSrc(src, options.srcDir);
+  options = _.extend(defaultOptions, options);
+  src = "./" + utilities.buildGulpSrc(src, options.srcDir);
+  dest = dest || config.jsOutput;
 
-    options = _.extend(defaultOptions, options);
+  gulp.task('webpack', function () {
 
-    gulp.task('webpack', function () {
+    var onError = function (e) {
+      new notification().error(e, 'Webpack Compilation Failed!');
+      this.emit('end');
+    };
 
-        var onError = function(e) {
-            new notification().error(e, 'Webpack Compilation Failed!');
-            this.emit('end');
-        };
+    var namedCb = function (file) {
+      file.named = file.relative.replace(/\.[^\.]+$/, '');
+      this.queue(file);
+    };
 
-        return gulp.src(src)
-            .pipe(webpack(options)).on('error', onError)
-            .pipe(gulpIf(! options.debug, uglify()))
-            .pipe(gulp.dest(options.outputDir))
-            .pipe(new notification().message('Webpack Compiled!'));
-    });
+    return gulp.src(src, { base: options.base })
+      .pipe(vinylNamed(namedCb))
+      .pipe(webpack(options.plugin)).on('error', onError)
+      .pipe(gulpIf(!options.debug, uglify()))
+      .pipe(gulp.dest(dest))
+      .pipe(new notification().message('Webpack Compiled!'));
+  });
 
-    this.registerWatcher('webpack', options.srcDir + '/**/*.js');
+  this.registerWatcher('webpack', options.srcDir + '/**/*.js');
 
-    return this.queueTask('webpack');
+  return this.queueTask('webpack');
 });
